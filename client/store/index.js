3ac2a9b4a4b745afb3783005fff2e226e4dc8878
Josh Remaley <joshremaley@gmail.com>
2018-08-27 15:07:34 -0500

    more setup

---
+++ client/store/index.js
@@ -0,0 +1,18 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import user from './user'
+
+const reducer = combineReducers({user})
+
+// const reducer = combineReducers({user, visits, parkList, pets, location, friends, events, friendsList})
+
+const middleware = composeWithDevTools(applyMiddleware(
+  thunkMiddleware,
+  createLogger({collapsed: true})
+))
+const store = createStore(reducer, middleware)
+
+export default store
+export * from './user'
