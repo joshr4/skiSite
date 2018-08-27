b3350859aeb4d87bd716b56e6516e36f9215b407
Josh Remaley <joshremaley@gmail.com>
2018-08-27 16:49:33 -0500

    docker setup

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,15 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false
-  }
-)
+const db = new Sequelize({
+  database: databaseName,
+  username: process.env.DATABASE_USER || null,
+  password: process.env.DATABASE_PASSWORD || null,
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: 5432,
+  logging: false
+})
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
