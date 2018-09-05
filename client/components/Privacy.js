795bf743f8cf506482ff2a3ff7ecffdc8a4c80aa
Josh Remaley <joshremaley@gmail.com>
2018-09-04 23:15:19 -0500

    temp privacy

--- client/components/Privacy.js
+++ client/components/Privacy.js
@@ -10,8 +10,33 @@ const Privacy = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
-      <p>Privacy</p>
+    <div style={{margin:'6% 0%', textAlign:'center'}}>
+      <h2>Privacy Policy</h2>
+      <p>This privacy policy discloses the privacy practices for the website. This privacy policy applies to information collected by this website online. Your privacy is important to us, and we will never share your personal information with 3rd party partners or advertisers. Note, this privacy policy is available under a Creative Commons Sharealike license, and was originally created by Automattic.com.
+
+
+
+Your privacy is critically important to us. At this website, we follow these principles for securing your information:
+
+We are thoughtful about the personal information we ask you to provide and the personal information that we collect about you through the operation of our services.
+
+We aim to make it as simple as possible for you to control what information on your website is shared publicly (or kept private), indexed by search engines, and permanently deleted.
+
+We aim for full transparency on how we gather, use, and share your personal information.
+
+​
+
+Below is our Privacy Policy, which incorporates and clarifies these principles.
+
+​
+
+Who We Are and What This Policy Covers
+
+Our mission is to empower you to do your best work. This Privacy Policy applies to information that we collect about you when you use:
+
+Our website and all associated pages, forms and mailing lists
+
+Any information collected via opt-in on Mailchimp landing page</p>
     </div>
   )
 }
